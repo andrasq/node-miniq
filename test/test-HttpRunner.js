767936941b01d2893653fcf7f03be9874049937d
Andras <andras@andrasq.com>
2020-08-05 21:09:08 -0400

    fix test: sync up http calls, then shut down server between tests

--- test/test-HttpRunner.js
+++ test/test-HttpRunner.js
@@ -9,11 +9,14 @@ var testUtils = require('../lib/testUtils');
 var Runner = require('../lib/Runner');
 var HttpRunner = require('../lib/HttpRunner');
 
+var port = 1337;
 module.exports = {
-    before: function(done) {
+    beforeEach: function(done) {
+        this.httpPort = port;
+        // port += 1;
         this.httpCalls = { count: 0, body: [] };
 
-        this.makeUrl = function makeUrl( path ) { return 'http://0.0.0.0:1337' + path };
+        this.makeUrl = function makeUrl( path ) { return 'http://0.0.0.0:' + this.httpPort + path };
 
         var self = this;
         this.httpServer = http.createServer(function(req, res) {
@@ -73,26 +76,31 @@ module.exports = {
                 self.httpCalls.count += 1;
             })
         })
-        this.httpServer.listen(1337, done);
-    },
 
-    beforeEach: function(done) {
+        this.waitMs = 20;
+        this.waitForCalls = function(ncalls, cb) {
+            var self = this;
+            if (this.httpCalls.count >= ncalls) setTimeout(cb, self.waitMs);
+            else setTimeout(function(){ self.waitForCalls(ncalls, cb) }, 1);
+        }
+
         this.httpCalls.count = 0;
         this.httpCalls.body = [];
         this.uut = new HttpRunner();
-        done();
+
+        this.httpServer.listen(this.httpPort, done);
     },
 
-    after: function(done) {
+    afterEach: function(done) {
         this.httpServer.close();
-        done();
+        setTimeout(done, 1);
     },
 
 
     'httpServer': {
         before: function(done) {
             this.makeUri = function makeUri( path, options ) { return utils.assignTo({
-                url: path[0] !== '/' ? path : 'http://0.0.0.0:1337' + path,
+                url: path[0] !== '/' ? path : this.makeUrl(path),
                 encoding: 'utf8',
                 method: 'POST',
             }, options || {}) };
@@ -100,6 +108,7 @@ module.exports = {
         },
 
         'returns a response': function(t) {
+            var self = this;
             microreq(this.makeUri('/echo?a=1&b=2', { encoding: 'json' }), function(err, res, body) {
                 t.ifError(err);
                 t.deepEqual(body, { a: 1, b: 2 });
@@ -144,23 +153,23 @@ module.exports = {
 
     'getRunningJobIds': {
         'returns ids of running jobs': function(t) {
-            var uut = this.uut;
-            uut.runJobs('type1', [{ id: 1, data: 'test1' }, { id: 2, data: 'test2' }], { body: this.makeUrl('/echo') }, t.ifError);
+            var self = this;
+            self.uut.runJobs('type1', [{ id: 1, data: 'test1' }, { id: 2, data: 'test2' }], { body: this.makeUrl('/echo') }, t.ifError);
             setTimeout(function() {
-                uut.getRunningJobIds(function(err, ids) {
+                self.uut.getRunningJobIds(function(err, ids) {
                     t.deepEqual(ids, [1, 2]);
-                    t.done();
+                    self.waitForCalls(2, t.done);
                 })
-            }, 5)
+            }, 2)
         },
 
         'returns ids of stopped but uncollected jobs': function(t) {
-            var uut = this.uut;
-            uut.runJobs('type1', [{ id: 1, data: 'test1' }, { id: 2, data: 'test2' }], { body: this.makeUrl('/echo') }, function(err) {
+            var self = this;
+            self.uut.runJobs('type1', [{ id: 1, data: 'test1' }, { id: 2, data: 'test2' }], { body: this.makeUrl('/echo') }, function(err) {
                 t.ifError(err);
-                uut.getRunningJobIds(function(err, ids) {
+                self.uut.getRunningJobIds(function(err, ids) {
                     t.deepEqual(ids, [1, 2]);
-                    t.done();
+                    self.waitForCalls(2, t.done);
                 })
             })
         },
@@ -170,12 +179,11 @@ module.exports = {
         'makes an http call': function(t) {
             var self = this;
             this.uut.runJobs('type1', [{ id: 1, data: 'test1' }], { body: this.makeUrl('/echo') }, t.ifError);
-            setTimeout(function() {
+            this.waitForCalls(1, function() {
                 // allow responses to arrive out of order (eg node-v0.8)
-                t.ok(/^test[12]\n$/.test(self.httpCalls.body[0]));
-                t.ok(/^test[12]\n$/.test(self.httpCalls.body[1]));
+                t.equal(self.httpCalls.body[0], 'test1\n');
                 t.done();
-            }, 20);
+            })
         },
 
         'returns stopped jobs': function(t) {
@@ -201,17 +209,12 @@ module.exports = {
 
         'runs many jobs': function(t) {
             var ncalls = 1000;
-            var waitMs = 40;
             var self = this;
             var jobs = [];
             for (var i = 0; i < ncalls; i++) jobs.push({ id: i, data: 'test-' + utils.pad(String(i), 6) });
             var startMs = Date.now();
             this.uut.runJobs('type-k', jobs, { body: this.makeUrl('/echo') }, t.ifError);
-            utils.repeatUntil(function(done) {
-                if (self.httpCalls.count < ncalls) return setTimeout(done, 1);
-                // allow 40 ms for the last response to be returned
-                setTimeout(done, waitMs, true);
-            }, function() {
+            self.waitForCalls(ncalls, function() {
                 t.equal(self.httpCalls.count, ncalls);
                 // cannot rely on call order, node-v0.8.28 runs them out of order
                 // t.deepEqual(self.httpCalls.body.slice(0, 3), ['test-000000', 'test-000001', 'test-000002']);
@@ -219,7 +222,7 @@ module.exports = {
                     t.ifError(err);
                     self.uut.getStoppedJobs(1e6, function(err, jobs2) {
 if (self.httpCalls.count !== ncalls) console.log("AR: wrong num calls", self.httpCalls.count, self.httpCalls.body.slice(0, 5), self.httpCalls.body.slice(-5));
-                        var doneMs = Date.now() - waitMs;
+                        var doneMs = Date.now() - self.waitMs;
                         t.ifError(err);
                         t.equal(jobs1.length, 10);
                         t.equal(jobs1[0].id, 0);
@@ -237,7 +240,6 @@ if (self.httpCalls.count !== ncalls) console.log("AR: wrong num calls", self.htt
         'runs batch of many jobs': function(t) {
 // FIXME: this test overlapped with previous test! first 5 call bodies contained previous calls plus our entire 1000-line batch!
             var ncalls = 1000;
-            var waitMs = 50;
             var self = this;
             var jobs = [];
             for (var i = 0; i < ncalls; i++) jobs.push({ id: i, data: 'test-' + utils.pad(String(i), 6) });
@@ -245,19 +247,18 @@ if (self.httpCalls.count !== ncalls) console.log("AR: wrong num calls", self.htt
             this.uut.runJobs('type-k', jobs, { body: this.makeUrl('/batchEcho'), options: { batch: true } }, function(err) {
                 t.ifError(err);
                 // jobs are off and running, wait for them to complete
-                utils.repeatUntil(function(done) {
-                    if (self.httpCalls.count < 1) return setTimeout(done, 1);
-                    setTimeout(done, waitMs, true);
-                }, function() {
+                self.waitForCalls(1, function() {
+//                    setTimeout(function() {
 if (self.httpCalls.count !== 1) console.log("AR: wrong num calls", self.httpCalls.count, self.httpCalls.body.slice(0, 5), self.httpCalls.body.slice(-5));
-                    self.uut.getStoppedJobs(ncalls, function(err, jobs) {
-                        var doneMs = Date.now() - waitMs;
-                        t.ifError(err);
-                        for (var i = 0; i < jobs.length; i++) t.equal(jobs[i].exitcode, 200);
-                        console.log("AR: total for batch of %d jobs: %d ms (jobs took %d ms avg each)", ncalls, doneMs - startMs, jobs[0].duration);
-                        // node-v10: 165k /batchEcho jobs/sec 10k (70-200k/s 1k)
-                        t.done();
-                    })
+                        self.uut.getStoppedJobs(ncalls, function(err, jobs) {
+                            var doneMs = Date.now() - self.waitMs;
+                            t.ifError(err);
+                            for (var i = 0; i < jobs.length; i++) t.equal(jobs[i].exitcode, 200);
+                            console.log("AR: total for batch of %d jobs: %d ms (jobs took %d ms avg each)", ncalls, doneMs - startMs, jobs[0].duration);
+                            // node-v10: 165k /batchEcho jobs/sec 10k (70-200k/s 1k)
+                            t.done();
+                        })
+//                    }, 40)
                 })
             })
         },
@@ -265,7 +266,7 @@ if (self.httpCalls.count !== 1) console.log("AR: wrong num calls", self.httpCall
         'errors': {
             'converts an http error to 500': function(t) {
                 var uut = this.uut;
-                uut.runJobs('type-E', [{ id: 99, data: 'test-E1' }], { body: 'http://localhost:1338/echo' }, t.ifError);
+                uut.runJobs('type-E', [{ id: 99, data: 'test-E1' }], { body: 'http://localhost:1336/echo' }, t.ifError);
                 setTimeout(function() {
                     uut.getStoppedJobs(10, function(err, jobs) {
                         t.ifError(err);
@@ -294,13 +295,13 @@ if (self.httpCalls.count !== 1) console.log("AR: wrong num calls", self.httpCall
 
         'batch errors': {
             'errors out batch jobs without a response': function(t) {
-                var uut = this.uut;
+                var self = this;
                 var jobs = [];
                 for (var i = 0; i < 6; i++) jobs.push({ id: String(i), data: String(i) });
-                uut.runJobs('type-B', jobs, { body: this.makeUrl('/halfBatch?size=2'), options: { batch: true } }, function(err) {
+                self.uut.runJobs('type-B', jobs, { body: this.makeUrl('/halfBatch?size=2'), options: { batch: true } }, function(err) {
                     t.ifError(err);
-                    setTimeout(function() {
-                        uut.getStoppedJobs(100, function(err, jobs) {
+                    self.waitForCalls(1, function() {
+                        self.uut.getStoppedJobs(100, function(err, jobs) {
                             t.ifError(err);
                             t.equal(jobs.length, 6);
                             t.deepEqual(jobs.map(function(j) { return j.exitcode }), [200, 200, 500, 500, 500, 500]);
@@ -308,37 +309,37 @@ if (self.httpCalls.count !== 1) console.log("AR: wrong num calls", self.httpCall
                                 undefined, undefined, 'NO_RESPONSE', 'NO_RESPONSE', 'NO_RESPONSE', 'NO_RESPONSE']);
                             t.done();
                         })
-                    }, 10)
+                    })
                 })
             },
 
             'errors out batch jobs after an invalid response': function(t) {
-                var uut = this.uut;
+                var self = this;
                 var jobs = [];
                 for (var i = 0; i < 4; i++) jobs.push({ id: String(i), data: String(i) });
-                uut.runJobs('type-B', jobs, { body: this.makeUrl('/badBatch?size=2'), options: { batch: true } }, function(err) {
+                self.uut.runJobs('type-B', jobs, { body: this.makeUrl('/badBatch?size=2'), options: { batch: true } }, function(err) {
                     t.ifError(err);
-                    setTimeout(function() {
-                        uut.getStoppedJobs(100, function(err, jobs) {
+                    self.waitForCalls(1, function() {
+                        self.uut.getStoppedJobs(100, function(err, jobs) {
                             t.ifError(err);
                             t.equal(jobs.length, 4);
                             t.deepEqual(jobs.map(function(j) { return j.exitcode }), [200, 200, 500, 500]);
                             t.deepEqual(jobs.map(function(j) { return j.code }), [undefined, undefined, 'INVALID_RESPONSE', 'INVALID_RESPONSE']);
                             t.done();
                         })
-                    }, 10)
+                    })
                 })
             },
 
             'errors out all jobs in batch on a request error': function(t) {
                 function noop(){};
-                var uut = this.uut;
+                var self = this;
                 var jobs = [];
                 for (var i = 0; i < 4; i++) jobs.push({ id: String(i), data: String(i) });
-                t.stub(uut, 'microreq').yields(new Error('mock request error')).returns({ write: noop, end: noop });
-                uut.runJobs('type-B', jobs, { body: this.makeUrl('/batchEcho'), options: { batch: true } }, function(err) {
+                t.stub(self.uut, 'microreq').yields(new Error('mock request error')).returns({ write: noop, end: noop });
+                self.uut.runJobs('type-B', jobs, { body: this.makeUrl('/batchEcho'), options: { batch: true } }, function(err) {
                     setTimeout(function() {
-                        uut.getStoppedJobs(100, function(err, jobs) {
+                        self.uut.getStoppedJobs(100, function(err, jobs) {
                             t.ifError(err);
                             t.equal(jobs.length, 4);
                             t.deepEqual(jobs.map(function(j) { return j.exitcode }), [500, 500, 500, 500]);
