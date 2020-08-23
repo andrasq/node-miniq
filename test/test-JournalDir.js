ee3eb8183a544e8bf4373a2064c38184b2179c6e
Andras <andras@andrasq.com>
2020-08-23 00:12:22 -0400

    start JournalDir tests

---
+++ test/test-JournalDir.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var fs = require('fs');
+var utils = require('../lib/utils');
+var JournalDir = require('../lib/JournalDir');
+
+module.exports = {
+    'makeLineReader': {
+        'returns a lineReader': function(t) {
+            var lineReader = JournalDir.makeLineReader(__filename);
+            t.equal(typeof lineReader.fgets, 'function');
+            t.equal(typeof lineReader.flush, 'function');
+            t.done();
+        },
+
+        'reads lines': function(t) {
+            var sourceFile = __filename;
+            var expect = fs.readFileSync(sourceFile);
+            var lines = [];
+            var lineReader = JournalDir.makeLineReader(sourceFile);
+            var startMs = Date.now();
+            utils.repeatUntil(function(done) {
+                var line = lineReader.fgets();
+                if (line !== undefined) lines.push(line);
+                done(lineReader.error, lineReader.eof);
+            }, function(err) {
+                var doneMs = Date.now();
+                t.ifError(err);
+                t.equal(lines.join('\n') + '\n', expect);
+                console.log("AR: read file %s in %d ms", sourceFile, doneMs - startMs);
+                t.ok(doneMs - startMs < 100);
+                t.done();
+            })
+        },
+    },
+}
