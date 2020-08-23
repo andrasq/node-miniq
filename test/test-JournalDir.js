06d0456093d0bdd1a4c5639a01d63941890381f3
Andras <andras@andrasq.com>
2020-08-23 18:14:41 -0400

    move makeLineReader out of JournalDir

--- test/test-JournalDir.js
+++ test/test-JournalDir.js
@@ -5,36 +5,4 @@ var utils = require('../lib/utils');
 var JournalDir = require('../lib/JournalDir');
 
 module.exports = {
-// FIXME: move into test-fileUtils.js
-    'makeLineReader': {
-        'returns a lineReader': function(t) {
-            var lineReader = JournalDir.makeLineReader(__filename);
-            t.equal(typeof lineReader.gets, 'function');
-            t.equal(typeof lineReader.flush, 'function');
-            t.done();
-        },
-
-        'reads lines': function(t) {
-            var sourceFile = __filename;
-            var expect = fs.readFileSync(sourceFile);
-            var lines = [];
-            var lineReader = JournalDir.makeLineReader(sourceFile);
-
-            var startMs = Date.now();
-            utils.repeatUntil(function(done) {
-                var line = lineReader.gets();
-                if (line !== undefined) lines.push(line);
-                done(lineReader.error, lineReader.isEof());
-            },
-            function(err) {
-                var doneMs = Date.now();
-                t.ifError(err);
-                var contents = lines.join('\n') + '\n';
-                t.equal(contents, expect);
-                console.log("AR: read file %s in %d ms", sourceFile, doneMs - startMs);
-                t.ok(doneMs - startMs < 100);
-                t.done();
-            })
-        },
-    },
 }
