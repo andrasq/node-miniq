94f0260cc0722f6d04b7ae3625d33823e688a082
Andras <andras@andrasq.com>
2020-08-23 20:26:14 -0400

    more fileUtils tests

--- test/test-fileUtils.js
+++ test/test-fileUtils.js
@@ -5,6 +5,22 @@ var utils = require('../lib/utils');
 var fileUtils = require('../lib/fileUtils');
 
 module.exports = {
+    'pathJoin builds paths': function(t) {
+        t.equal(fileUtils.pathJoin('foo', 'bar'), 'foo/bar');
+        t.equal(fileUtils.pathJoin('foo', 'bar/baz.js'), 'foo/bar/baz.js');
+        t.done();
+    },
+
+    'fileExists checks file existence': function(t) {
+        t.ok(fileUtils.fileExists(__filename));
+        t.ok(fileUtils.fileExists(__dirname));
+        t.ok(!fileUtils.fileExists('/nonesuch'));
+        t.done();
+    },
+
+    'waitWhile': {
+    },
+
     'makeLineReader': {
         'returns a lineReader': function(t) {
             var lineReader = fileUtils.makeLineReader(__filename);
@@ -35,5 +51,61 @@ module.exports = {
                 t.done();
             })
         },
+    },
+
+    'grabFile': {
+        beforeEach: function(done) {
+            this.temp1 = '/tmp/test.' + process.pid + '.tmp1';
+            this.temp2 = '/tmp/test.' + process.pid + '.tmp2';
+            try { fs.unlinkSync(this.temp1) } catch (e) {}
+            try { fs.unlinkSync(this.temp2) } catch (e) {}
+            done();
+        },
+
+        afterEach: function(done) {
+            try { fs.unlinkSync(this.temp1) } catch (e) {}
+            try { fs.unlinkSync(this.temp2) } catch (e) {}
+            done();
+        },
+
+        'renames file': function(t) {
+            var self = this;
+            fs.writeFileSync(self.temp1, 'test ' + process.pid);
+            fileUtils.grabFile(self.temp1, self.temp2, function(err) {
+                t.ifError(err);
+                t.equal(fs.readFileSync(self.temp2), 'test ' + process.pid);
+                t.done();
+            })
+        },
+
+        'ensure that grabbed file exists': function(t) {
+            var self = this;
+            fileUtils.grabFile(self.temp1, self.temp2, function(err) {
+                t.ifError(err);
+                t.ok(!fileUtils.fileExists(self.temp1));
+                t.ok(fileUtils.fileExists(self.temp2));
+                t.equal(fs.readFileSync(self.temp2), '');
+                t.done();
+            })
+        },
+    },
+
+    'concatFiles concatenates and removes files': function(t) {
+        var self = this;
+        var pid = process.pid;
+        var names = ['t1.' + pid, 't2.' + pid, 't3.' + pid];
+        var targetpath = '/tmp/tt.' + pid;
+        fs.writeFileSync('/tmp/t1.' + pid, 'line1\n');
+        fs.writeFileSync('/tmp/t2.' + pid, 'line2\n');
+        fs.writeFileSync('/tmp/t3.' + pid, 'line3\n');
+        fileUtils.concatFiles(targetpath, '/tmp', names, function(err) {
+            t.ifError(err);
+            t.equal(fs.readFileSync(targetpath), 'line1\nline2\nline3\n');
+            t.ok(!fileUtils.fileExists('/tmp/t1.' + pid));
+            t.ok(!fileUtils.fileExists('/tmp/t2.' + pid));
+            t.ok(!fileUtils.fileExists('/tmp/t3.' + pid));
+            fs.unlinkSync(targetpath);
+            t.done();
+        })
     },
 }
