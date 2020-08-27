afd0f5e27aa447f9aa3f701de75496bf0a27d26f
Andras <andras@andrasq.com>
2020-08-26 20:51:54 -0400

    utils.Mutex, alternate to waitWhile

--- test/test-utils.js
+++ test/test-utils.js
@@ -867,6 +867,55 @@ console.log("AR: got %d ids in %d ms, %d/ms", ids.length, t2 - t1, (ids.length /
         },
     },
 
+    'Mutex': {
+        'runs calls immediately': function(t) {
+            var uut = new utils.Mutex();
+            var t1 = Date.now();
+            uut.acquire(function(release) {
+                var t2 = Date.now();
+                t.ok(t2 - t1 <= 1);
+                release();
+                t.done();
+            })
+        },
+
+        'sets busy when in use': function(t) {
+            var uut = new utils.Mutex();
+            uut.acquire(function(release) {
+                t.ok(uut.busy);
+                release();
+                t.done();
+            })
+        },
+
+        'clears busy when no longer in use': function(t) {
+            var uut = new utils.Mutex();
+            uut.acquire(function(release) {
+                t.ok(uut.busy);
+                setTimeout(function() {
+                    t.ok(!uut.busy);
+                    t.done();
+                }, 2)
+                release();
+            })
+        },
+
+        'queues calls': function(t) {
+            var uut = new utils.Mutex();
+            var call1, call2, t1 = Date.now();
+            uut.acquire(function(release) { call1 = true; setTimeout(release, 5) });
+            uut.acquire(function(release) { call2 = true; setTimeout(release, 5) });
+            uut.acquire(function(release) {
+                var t2 = Date.now();
+                t.ok(t2 - t1 >= 10 - 1);
+                t.ok(call1);
+                t.ok(call2);
+                release();
+                t.done();
+            })
+        },
+    },
+
     'toStruct': {
         'returns the struct': function(t) {
             var x = {};
